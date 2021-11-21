cb47fd141a64b64eba240a19e55528c04bf3d726
Harsh Singh <kmrharsh50@gmail.com>
2021-11-21 22:35:48 +0530

    [Harsh]Added:Adding to LinkList DataStructure Implemented

---
+++ DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Welcome to Linked List Problems!");
+                LinkList ln = new LinkList();
+                ln.Add(56);
+                ln.Add(30);
+                ln.Add(70);
+                ln.Display();
+
+            }
+        }
+}
