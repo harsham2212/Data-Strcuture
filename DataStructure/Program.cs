7de65b8fab547143a7230892d2bbfece927b87fa
Harsh Singh <kmrharsh50@gmail.com>
2021-11-22 09:22:30 +0530

    [Harsh]Added:deleting Last Node of LinkList DataStructure Implemented

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructure
                 ln.Add(56);
                 ln.Add(30);
                 ln.Add(70);
-                ln.RemoveFirstNode();
+                ln.RemoveLastNode();
                 Console.WriteLine("After removing is : ");
                 ln.Display();
         }
