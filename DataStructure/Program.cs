f7f84ce0413cb3dbbeb3db779c7703f76b9eece2
Harsh Singh <kmrharsh50@gmail.com>
2021-11-22 09:18:51 +0530

    [Harsh]Added:deleting First Node of LinkList DataStructure Implemented

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,6 +11,8 @@ namespace DataStructure
                 ln.Add(56);
                 ln.Add(30);
                 ln.Add(70);
+                ln.RemoveFirstNode();
+                Console.WriteLine("After removing is : ");
                 ln.Display();
         }
         }
