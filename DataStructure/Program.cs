d161b609f67a19faa614427c886438625da1b346
Harsh Singh <kmrharsh50@gmail.com>
2021-11-22 08:43:39 +0530

    [Harsh]Added:Appendng LinkList DataStructure Implemented

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,6 +11,8 @@ namespace DataStructure
                 ln.Add(56);
                 ln.Add(30);
                 ln.Add(70);
-            }
+                ln.InsertAtParticularPosition(2, 30);
+                ln.Display();
+        }
         }
 }
